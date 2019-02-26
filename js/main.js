ac125d970a90bc3f02feb955ac580666a3e272d5
obayomi96 <martinsoluwaseun47@gmail.com>
2019-02-26 16:54:35 +0100

    20 seconds countdown timer

--- js/main.js
+++ js/main.js
@@ -23,29 +23,31 @@ function digitalClock() {
 setInterval(digitalClock, 1000);
 
 // 20 minutes countdown timer
-// function countdown( elementName, minutes, seconds ) {
-//     var element, endTime, hours, mins, msLeft, time;
+function countdown(elementName, minutes, seconds) {
+  let element, endTime, hours, mins, msLeft, time;
+  // Make countdown double digits when less that 10
+  function twoDigits(n) {
+    return n <= 9 ? "0" + n : n;
+  }
+  // Timer counter update
+  function updateTimer() {
+    msLeft = endTime - +new Date();
+    if (msLeft < 1000) {
+      element.innerHTML = "Exercise Complete";
+    } else {
+      time = new Date(msLeft);
+      hours = time.getUTCHours();
+      mins = time.getUTCMinutes();
+      element.innerHTML =
+        (hours ? "0" + ":" + twoDigits(mins) : mins) +
+        ":" +
+        twoDigits(time.getUTCSeconds());
+      setTimeout(updateTimer, time.getUTCMilliseconds() + 500);
+    }
+  } // updateTimer function end
 
-//     function twoDigits( n ) {
-//         return (n <= 9 ? "0" + n : n);
-//     }
-
-//     function updateTimer() {
-//         msLeft = endTime - (+new Date);
-//         if ( msLeft < 1000 ) {
-//             element.innerHTML = "Take your 20-20-20 exercise now";
-//         } else {
-//             time = new Date( msLeft );
-//             hours = time.getHours();
-//             mins = time.getMinutes();
-//             element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.get() );
-//             setTimeout( updateTimer, time.getMilliseconds() + 500 );
-//         }
-//     }
-
-//     element = document.getElementById( elementName );
-//     endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
-//     updateTimer();
-// } // countdown function end
-
-// countdown( "countdown", 20, 0 );
+  element = document.getElementById(elementName);
+  endTime = +new Date() + 1000 * (60 * minutes + seconds) + 500;
+  updateTimer();
+} // countdown function end
+countdown("countdown-timer", 0, 20);
